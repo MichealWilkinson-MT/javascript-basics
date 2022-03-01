9e95944f30d15667cce807e30c10653aaf8ddad0
Wes <76014409+Redtigercod4@users.noreply.github.com>
2022-03-01 10:05:38 +0000

    sumNumbers Completed
    Two solutions attached

--- src/arrays.js
+++ src/arrays.js
@@ -1,6 +1,6 @@
 const getNthElement = (index, array) => {
   while (index >= array.length) {
-    index=index-array.length;
+    index = index - array.length;
   }
   return array[index]
 };
@@ -13,24 +13,24 @@ const csvStringToArray = string => {
   return string.split(",")
 };
 
-const addToArray = (element, array) => {   
+const addToArray = (element, array) => {
   array.push(element)
 };
 
-const addToArray2 = (element, array) => {   
+const addToArray2 = (element, array) => {
   let array2 = [...array];
   array2.push(element)
-    return array2;
+  return array2;
 };
 
-const removeNthElement = (index, array) => {   
+const removeNthElement = (index, array) => {
   array.splice(index, 1)
   return array;
 };
 
 const numbersToStrings = numbers => {
   let output = []
-  for (i = 0; i < numbers.length; i++){
+  for (i = 0; i < numbers.length; i++) {
     const number = numbers[i]
     output.push(number.toString())
   }
@@ -56,13 +56,21 @@ const removeNthElement2 = (index, array) => {
 const elementsStartingWithAVowel = strings => {
   // your code here
 };
-
+// let total = numbers.reduce(function(previousValue, currentValue){
+//   return previousValue  + currentValue 
+// })
+// return total
 const removeSpaces = string => {
   // your code here
 };
 
 const sumNumbers = numbers => {
-  // your code here
+  let sum = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum
 };
 
 const sortByLastLetter = strings => {
